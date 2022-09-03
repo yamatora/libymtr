f4bc3300f0bb3e32a85d96c72aea507fc9e4c80c
yamatora <yt@yamadataiga.com>
2022-09-04 03:22:02 +0900

    IP, Udp

--- src/libymtr/Network/IP.cs
+++ src/libymtr/Network/IP.cs
@@ -9,17 +9,40 @@ namespace libymtr.Network {
         /// </summary>
         /// <returns></returns>
         public static IPAddress? GetLocalIP() {
-            foreach(var iface in NetworkInterface.GetAllNetworkInterfaces()) {
+            foreach (var iface in NetworkInterface.GetAllNetworkInterfaces()) {
                 //  Ethernet or Wi-Fi
                 var type = iface.NetworkInterfaceType;
-                if(type != NetworkInterfaceType.Ethernet && type != NetworkInterfaceType.Wireless80211) {
+                if (type != NetworkInterfaceType.Ethernet && type != NetworkInterfaceType.Wireless80211) {
                     continue;
                 }
-                foreach(var ip in iface.GetIPProperties().UnicastAddresses) {
-                    if(ip.Address.AddressFamily != AddressFamily.InterNetwork || ip.PrefixOrigin != PrefixOrigin.Dhcp) {
+                foreach (var ip in iface.GetIPProperties().UnicastAddresses) {
+                    if (ip.Address.AddressFamily != AddressFamily.InterNetwork || ip.PrefixOrigin != PrefixOrigin.Dhcp) {
                         continue;
                     }
                     return ip.Address;
+                }
+            }
+            return null;
+        }
+        public static string? GetBroadcastAddress() {
+            foreach (var iface in NetworkInterface.GetAllNetworkInterfaces()) {
+                //  Ethernet or Wi-Fi
+                var type = iface.NetworkInterfaceType;
+                if (type != NetworkInterfaceType.Ethernet && type != NetworkInterfaceType.Wireless80211) {
+                    continue;
+                }
+                foreach (var ip in iface.GetIPProperties().UnicastAddresses) {
+                    if (ip.Address.AddressFamily != AddressFamily.InterNetwork || ip.PrefixOrigin != PrefixOrigin.Dhcp) {
+                        continue;
+                    }
+                    string[] local = ip.Address.ToString().Split('.');
+                    string[] smask = ip.IPv4Mask.ToString().Split('.');
+                    string[] result = new string[4];
+                    for (int i = 0; i < 4; i++) {
+                        byte bresult = (byte)(byte.Parse(local[i]) | ~byte.Parse(smask[i]));
+                        result[i] = bresult.ToString();
+                    }
+                    return Generic.ConcatByChar('.', result);
                 }
             }
             return null;
