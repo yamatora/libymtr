0e588a1331c25d584402161ee2865b0241818550
yamatora <yt@yamadataiga.com>
2022-09-06 12:54:15 +0900

    Implement WebClient

--- src/libymtr/Network/WebClient.cs
+++ src/libymtr/Network/WebClient.cs
@@ -3,8 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace libymtr.Network {
+    public delegate void ResponseHandler(HttpResponseMessage message, bool isErro = false);
+
     internal class WebClient {
+
+
+        public event ResponseHandler OnResponse;
+
+        private static HttpClient m_client = null;
+        private CancellationTokenSource m_canceller = new CancellationTokenSource();
+
+        public WebClient(int timeoutSec = 60) {
+            OnResponse += _OnResponse;
+        }
+        ~WebClient() {
+            m_client.Dispose();
+        }
+
+        public async void SendAsync(HttpRequestMessage request) {
+            try {
+                var response = await m_client.SendAsync(request, m_canceller.Token);
+                OnResponse(response);
+            } catch {
+                Cancel();
+                HttpResponseMessage msg = new HttpResponseMessage();
+                msg.Content = new StringContent("Network Error");
+                OnResponse(msg, true);
+            }
+        }
+        public void Cancel() {
+            m_canceller.Cancel();
+            m_canceller = new CancellationTokenSource();
+        }
+
+        private void _OnResponse(HttpResponseMessage message, bool isError) {
+            //  Empty event
+        }
     }
 }
